e60efba1334b9cf67b971ead27857d5baa4af75d
Ruslan <ruslan.grudistov@gmail.com>
2025-02-26 13:15:33 +0200

    1

--- task_2.py
+++ task_2.py
@@ -1,35 +1,47 @@
 import json
 from datasketch import HyperLogLog
-import timeit
+import time
+import pandas as pd 
 
 file = "./lms-stage-access.log"
 
+hll = HyperLogLog(p=10)
 set_data = set()
-hll = HyperLogLog(p=14)
 
 with open(file, "r", encoding="utf-8") as f:
-        for line in f:
-            try:
-                log_entry = json.loads(line)  
-                ip = log_entry.get("remote_addr")
-                if str(ip):  
-                    set_data.add(ip)
-                    hll.update(ip.encode("utf-8"))
+    for line in f:
+        try:
+            log_entry = json.loads(line)  
+            ip = log_entry.get("remote_addr")  
+            if isinstance(ip, str):  
+                set_data.add(ip)    
+                hll.update(ip.encode("utf-8"))
 
-            except json.JSONDecodeError:
-                continue  
+        except json.JSONDecodeError:
+            continue  
 
-def count_unique_ips_set(data):     
+def count_unique_ips_set(data):         
     return len(data)
 
 
-def count_unique_ips_hyperloglog(data):    
+def count_unique_ips_hyperloglog(data): 
     return data.count()
 
 
 
-time_set = timeit.timeit(lambda: count_unique_ips_set(set_data), number=1)
-time_hyperloglog = timeit.timeit(lambda: count_unique_ips_hyperloglog(hll), number=1)
+start_time_set = time.perf_counter()
+unique_ips_set = count_unique_ips_set(set_data)
+end_time_set = time.perf_counter()
 
-print(f"Час виконанная методу Set(): {time_set} \n Унікальних IP-адрес: {count_unique_ips_set(set_data)}")
-print(f"Час виконанная методу Hyperloglog: {time_hyperloglog} \n Унікальних IP-адрес: {count_unique_ips_hyperloglog(hll)}")
+start_time_hll = time.perf_counter()
+unique_ips_hll = count_unique_ips_hyperloglog(hll)
+end_time_hll = time.perf_counter()
+
+df = pd.DataFrame({
+    "Метод": ["Точний підрахунок", "HyperLogLog"],
+    "Унікальні елементи": [unique_ips_set, unique_ips_hll],
+    "Час виконання (сек.)": [end_time_set-start_time_set, end_time_hll-start_time_hll]
+})
+
+
+print(df.to_string(index=False))
